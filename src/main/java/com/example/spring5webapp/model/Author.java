94013ca151e6ff05373550aefd61053c8997bf59
Romil Badhe <romilbadhe18@gmail.com>
2017-12-03 01:22:44 +0530

    JPA Enitiy @ManyToMany

---
+++ src/main/java/com/example/spring5webapp/model/Author.java
@@ -0,0 +1,42 @@
+package com.example.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+}
