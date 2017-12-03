8907656393348d27701400a476506d5072f390b8
Romil Badhe <romilbadhe18@gmail.com>
2017-12-03 22:44:23 +0530

    Publisher Entity Added with @OneToOne Mapping with Book Entity

--- src/main/java/com/example/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/example/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.example.spring5webapp.bootstrap;
 
 import com.example.spring5webapp.model.Author;
 import com.example.spring5webapp.model.Book;
+import com.example.spring5webapp.model.Publisher;
 import com.example.spring5webapp.repositories.AuthorRepository;
 import com.example.spring5webapp.repositories.BookRepository;
+import com.example.spring5webapp.repositories.PublisherRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
@@ -17,11 +19,15 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
     // Constructor AutoInjection to AutoWire the Repositories
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -33,10 +39,20 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData(){
 
+        Publisher apress = new Publisher();
+        apress.setName("Apress Inc");
+        publisherRepository.save(apress);
+        LOGGER.info("Apress Publisher Created");
+
+        Publisher wrox = new Publisher();
+        wrox.setName("Wrox Inc");
+        publisherRepository.save(wrox);
+        LOGGER.info("Wrox Publisher Created");
+
         // Adding John
         LOGGER.info("Adding John");
         Author john = new Author("John", "Doe");
-        Book ddd = new Book("Domain Driven Design", "1234", "Haper Collins");
+        Book ddd = new Book("Domain Driven Design", "1234", apress);
         john.getBooks().add(ddd);
         ddd.getAuthors().add(john);
         authorRepository.save(john);
@@ -46,7 +62,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         // Adding Rod
         LOGGER.info("Adding Rod");
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "2244", "Worx");
+        Book noEJB = new Book("J2EE Development without EJB", "2244", wrox);
         rod.getBooks().add(noEJB);
         authorRepository.save(rod);
         bookRepository.save(noEJB);
